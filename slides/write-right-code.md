2183f46aeb6d4b242fb9d3abf524de671c2e5ed4
Ashley Trinh <ashley@hackbrightacademy.com>
2021-08-05 17:39:11 -0700

    I have no idea what's going on here. SORRY.

---
+++ slides/write-right-code.md
@@ -0,0 +1,11 @@
+---
+date: 2021-06-03
+title: Write Right Code
+summary: Wew this is a summary wow
+---
+
+<iframe
+    width="100%"
+    height="400"
+    src="/static/slides/write-right-code/index.html">
+</iframe>
--- src/filters.js
+++ src/filters.js
@@ -4,12 +4,15 @@ const path = require("path");
 
 const DATE_FORMAT = "dddd, MMM Do YYYY";
 
-exports.displayDate = (date) => moment(date.toUTCString()).format(DATE_FORMAT);
+exports.displayDate = (date) =>
+  moment(date.toISOString().slice(0, 10)).format(DATE_FORMAT);
 
 exports.toMarkdown = (val) =>
   typeof val === "string" ? new markdownit().render(val) : val;
 
 exports.includes = (arr, obj) => arr.includes(obj);
+
+exports.limit = (arr, limit) => arr.slice(0, limit);
 
 exports.getParentURL = (
   page // Second-to-last element is parent URL
